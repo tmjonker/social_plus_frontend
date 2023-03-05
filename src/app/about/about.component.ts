b58f9ba480009373ce695f3054af0aaa919c5cb6
tmjonker <tmjonker1@outlook.com>
2023-03-04 20:47:52 -0500

    Fixed a bunch of stuff and created an about page.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-about',
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,29 +1,45 @@
 import { Pages } from './../pages';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnDestroy, OnInit {
   pages!: Pages;
 
   constructor() {
-    this.pages = {
-      signIn: true,
-      register: false,
-      about: false,
+    this.pages = JSON.parse(sessionStorage.getItem('pages')!);
+
+    console.log(this.pages);
+    if (this.pages === null) {
+      this.pages = {
+        signIn: true,
+        register: false,
+        about: false,
+      };
+    } else {
+      this.pages = {
+        signIn: this.pages.signIn,
+        register: this.pages.register,
+        about: this.pages.about,
+      };
     }
   }
-  
+  ngOnInit(): void {
+    window.onbeforeunload = () => this.ngOnDestroy();
+  }
+  ngOnDestroy(): void {
+    sessionStorage.setItem('pages', JSON.stringify(this.pages));
+  }
+
   toggleRegister() {
     this.pages = {
       signIn: false,
       register: true,
       about: false,
-    }
+    };
   }
 
   toggleAbout() {
@@ -31,7 +47,7 @@ export class NavbarComponent {
       signIn: false,
       register: false,
       about: true,
-    }
+    };
   }
 
   toggleSignIn() {
@@ -39,6 +55,6 @@ export class NavbarComponent {
       signIn: true,
       register: false,
       about: false,
-    }
+    };
   }
 }
